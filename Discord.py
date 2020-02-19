737adb0879034bce626eb97f8c857757334217bb
Robert Kurcwald <r.kurcwald@gmail.com>
2020-02-19 23:58:54 +0100

    V0.01
    1. Connecting to Discord by token.
    2. Commands menu.

---
+++ Discord.py
@@ -0,0 +1,42 @@
+import discord
+from discord.ext.commands import Bot
+
+TOKEN = 'NTI2Mzk0NzUxMjc3MjAzNDg2.XkkFgA.xqV-hNpg-ZBh93soSe1VfG6KPAA'
+
+"""client = discord.Client()"""
+my_bot = Bot(command_prefix="!")
+
+
+@my_bot.command(name='help1',
+                description='Pomoc i podstawowe informacje o serwerze',
+                aliases=['h','info'])
+async def help(ctx):
+    helpArray='Witamy {0.author.mention}'.format(ctx)+' w dziale pomocy.\nWpisz !commands w celu sprawdzenia komend\nWpisz !reg w celu sprawdzenia regulaminu\nMiłych rozmów ;)'
+    await ctx.message.channel.send(helpArray)
+
+
+@my_bot.command(name='commands',
+                description='Lista komend',
+                aliases=['cmd'])
+async def commands(ctx):
+    commandsArray='!help [!h] - Podstawowe informacje\n!commands [!cmd] - Komendy\n!skroty [!als] - Skrotowe komendy'
+    await ctx.message.channel.send(commandsArray)
+
+
+@my_bot.command(name='aliases',
+               description='Lista aliasow komend',
+               aliases=['als','skroty'],
+                color='red')
+async def aliases(ctx):
+    aliasesArray='!help <=> !help, !h, !info\n\!commands <=> !commands, !cmd\n\!register <=> !register, !reg\n!aliases <=> !aliases, !als, !skroty\n'
+    await ctx.message.channel.send(aliasesArray)
+
+
+@my_bot.event
+async def on_ready():
+    print('Logged in as')
+    print(my_bot.user.name)
+    print(my_bot.user.id)
+    print('------')
+
+my_bot.run(TOKEN)
